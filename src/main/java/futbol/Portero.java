7e88569c1e648333e17ca6ff18952e40f6aeff99
IThiagoRo <isrojasm@unal.edu.co>
2020-10-19 21:19:46 -0500

    Solution1

---
+++ src/main/java/futbol/Portero.java
@@ -0,0 +1,36 @@
+package futbol;
+
+public class Portero extends Futbolista{
+	public short golesRecibidos;
+	public byte dorsal;
+	
+	public Portero(String nombre, int edad, short golesRecibidos, byte dorsal) {
+		super(nombre, edad, "Portero");
+		this.golesRecibidos = golesRecibidos; 
+		this.dorsal = dorsal;
+	}
+	
+	public int compareTo(Portero portero) {
+		return portero.getGolesRecibidos() -this.getGolesRecibidos();
+	}
+	
+	
+	public String toString() {
+		return "El futbolista " + this.getNombre() + " tiene " + this.getEdad() + ", y juega de " + this.getPosicion() +
+		    	" con el dorsal " + this.getDorsal() + ". Le han marcado " + this.getGolesRecibidos();
+	}
+	
+	public short getGolesRecibidos() {
+		return this.golesRecibidos;
+	}
+	
+	
+	public byte getDorsal() {
+		return this.dorsal;
+	}
+	
+	public boolean jugarConLasManos() {
+		return true;
+	}
+	
+}
